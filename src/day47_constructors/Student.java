c58c6a1ba0efd410155a8ca1f4ee88e45c87e1fb
Serghei Vlaicu <82963033+vlaicu89@users.noreply.github.com>
2021-05-27 20:57:35 -0500

    updated or added constructors to Address.java and created addressList.java 2

---
+++ src/day47_constructors/Student.java
@@ -0,0 +1,10 @@
+package day47_constructors;
+
+public class Student {
+    public Student(){
+        System.out.println("No-Args Constructor");
+    }
+    public Student(String name){
+        System.out.println("name param constructor= " + name);
+    }
+}
---
+++ src/day47_constructors/StudentObject.java
@@ -0,0 +1,11 @@
+package day47_constructors;
+
+public class StudentObject {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3 = new Student("Akbar");
+        Student st4= new Student("Serghei");
+    }
+
+}
