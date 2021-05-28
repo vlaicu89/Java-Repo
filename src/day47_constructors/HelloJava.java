ef9ffd4092b5ded701a273c823d7a24d445cc678
Serghei Vlaicu <82963033+vlaicu89@users.noreply.github.com>
2021-05-28 10:35:48 -0500

    update1

--- src/day47_constructors/HelloJava.java
+++ src/day47_constructors/HelloJava.java
@@ -3,5 +3,6 @@ package day47_constructors;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("jdfbvjsfhiwekjfmdc");
     }
 }
