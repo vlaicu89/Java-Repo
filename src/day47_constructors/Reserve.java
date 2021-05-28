2fb9c1e76c917614e1554e60d7951d9c1625bcd3
Serghei Vlaicu <82963033+vlaicu89@users.noreply.github.com>
2021-05-28 11:10:59 -0500

    up2

--- src/day47_constructors/Reserve.java
+++ src/day47_constructors/Reserve.java
@@ -1,8 +1,7 @@
 package day47_constructors;
 
-public class Class0000 {
+public class Reserve {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hi");
     }
-
 }
